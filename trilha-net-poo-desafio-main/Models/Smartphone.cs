963f801980b9d937b1cd065c39cc7fa417e6edd3
Carlos Garcia <111895268+Carlos-CGS@users.noreply.github.com>
2023-03-23 13:07:15 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Models/Smartphone.cs
@@ -0,0 +1,33 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone 
+    {
+
+        public string Numero { get; set; } 
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get;}
+        protected string Imei { get;}
+        protected int Memoria { get;}
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
