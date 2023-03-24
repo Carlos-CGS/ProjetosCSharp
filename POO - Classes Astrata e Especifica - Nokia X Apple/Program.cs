55c98d208bdf4971915b465a5b036b9a318e22e3
Carlos Garcia <111895268+Carlos-CGS@users.noreply.github.com>
2023-03-24 09:03:53 -0300

    Update Program.cs

--- POO - Classes Astrata e Especifica - Nokia X Apple/Program.cs
+++ POO - Classes Astrata e Especifica - Nokia X Apple/Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"modelo 01", imei:"111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Iphone: ");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "modelo 02", imei: "222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
