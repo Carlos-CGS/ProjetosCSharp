ee2c892b0c19954520a3814ba350acb09ba50f1f
Carlos Garcia <111895268+Carlos-CGS@users.noreply.github.com>
2023-03-27 13:20:08 -0300

    Add files via upload

---
+++ If Ternário/README.md
@@ -0,0 +1,3 @@
+# If Ternário
+- Foi criada um Booleano (ehPar), para verficar se um numero é par ou impar.
+- Na variavel que imprimi o resultado foi criado um If Ternário utilizando este booleano, que informa se o numero é par ou impar.
---
+++ If Ternário/ifTernario.cs
@@ -0,0 +1,9 @@
+double numero = 10.0;
+bool ehPar = numero % 2 == 0;
+Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "impar"));
+
+double numero2 = 15.0;
+bool ehPar2 = numero2 % 2 == 0;
+Console.WriteLine($"O número {numero2} é " + (ehPar2 ? "par" : "impar"));
+
+
